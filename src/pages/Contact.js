ab606bc87794a265521678d834c1a9e945550bb4
Since30 <102620248+Since30@users.noreply.github.com>
2023-05-10 13:28:21 +0200

    Update Contact.js
    Maj Contact pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,8 @@
 import React from "react";
+import { FormControl } from "../test/form-control";
 export default function Contact() {
 
     return (
-
-        <div className="contact">
-            <h1>Contact</h1>
-            <div>
-                <form className="form1">
-                    <label htmlFor="name">Nom</label>
-                    <input type="text" id="name" name="name" />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" />
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" />
-                    <button type="submit">Envoyer</button>
-                </form>
-            </div>
-        </div>
+        <FormControl />
     );
 }
